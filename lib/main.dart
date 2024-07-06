fd1febfb0afc10ecb14dde7e6990a888c878bd6f
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2024-07-06 15:47:53 +0300

    launch misha app

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:misha/misha_app.dart';
+
+void main() {
+  runApp(const MishaApp());
+}
---
+++ lib/misha_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MishaApp extends StatelessWidget {
+  const MishaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
